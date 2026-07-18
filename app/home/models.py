b8cd16695e593ab0757ebeb0f03c52a2cc89e569
Nick Moreton <nickmoreton@me.com>
2026-07-18 19:47:57 +0100

    feat: render blog pages and homepage link

--- app/home/models.py
+++ app/home/models.py
@@ -4,4 +4,11 @@ from wagtail.models import Page
 
 
 class HomePage(Page):
-    pass
+    def get_context(self, request, *args, **kwargs):
+        from app.blog.models import BlogIndexPage
+
+        context = super().get_context(request, *args, **kwargs)
+        context["blog_page"] = (
+            BlogIndexPage.objects.child_of(self).live().public().first()
+        )
+        return context
